7c26dcea12538cf726e28f0542718d9bf97983b3
vonvick <victor.nwaiwu9@gmail.com>
2016-10-18 14:10:38 +0100

    add chalk, clear and figlet method to the application

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 // require dependencies that are needed to run the app
 require('dotenv').config();
+var clear = require('clear');
+var chalk = require('chalk');
+var figlet = require('figlet');
 var readline = require('readline');
 var request  = require('request');
 var Twitter  = require('twitter');
@@ -13,13 +16,20 @@ var tweet = new Twitter({
   access_token_secret:process.env.access_token_secret
 });
 
-console.log('Welcome to the WEATHER and TWEET API');
+clear();
+console.log(
+  chalk.yellow(
+    figlet.textSync('Weather & Twitter API', { horizontalLayout: 'quarter' })
+  )
+);
+console.log(chalk.blue('Welcome to the WEATHER and TWEET API'));
 
 // function that is called at the beginning of the application
 function intro() {
-  console.log('Press "1" to get the weather of a location');
-  console.log('Press "2" to post a tweet on tweeter');
-  console.log('Type "exit" to exit the application');
+  console.log(chalk.yellow('Press "1" to get the weather of a location'));
+  console.log(chalk.yellow('Press "2" to post a tweet on tweeter'));
+  console.log(chalk.yellow('Type "exit" to exit the application'));
+  console.log('');
 }
 intro();
 
@@ -35,34 +45,36 @@ read.on('line',(line) => {
     // displays the weather information of a place given by the user
     case '1':
       read.question('Enter the name of the city you want to know the weather for: ', function(answer) {
-        // put in your api key in the part with "xxxxxxxxxxxxxxxxx"
-        var link = "http://api.openweathermap.org/data/2.5/weather?q="+answer+"&mode=json&appid="+process.env.openweather_key;
-        var results = "";
-        request({
-          url: link,
-          qs: {from: 'Open Weather', time: +new Date()},
-          method: 'GET',
-          headers: {
-              'Content-Type': 'application/json; charset=utf-8'
+          // put in your api key in the part with "xxxxxxxxxxxxxxxxx"
+          var link = "http://api.openweathermap.org/data/2.5/weather?q="+answer+"&mode=json&appid="+process.env.openweather_key;
+          var results = "";
+          request({
+            url: link,
+            qs: {from: 'Open Weather', time: +new Date()},
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8'
+              }
+          }, function(error, response, body){
+            if(error) {
+              console.log(chalk.red(error));
+              intro();
+            } else if (response.statusCode === 200) {
+              var object = JSON.parse(body);
+              var temp = Math.round(object.main.temp -273); 
+              console.log(chalk.green('Your status code is: ',response.statusCode));
+              console.log(chalk.green('The city name is: ', object.name));
+              console.log(chalk.green('The weather will have: ', object.weather[0].description));
+              console.log(chalk.green('The temperature is: ', temp, 'celcius'));
+              console.log(chalk.green('The humidity is: ', object.main.humidity, '%'));
+              console.log('');
+              intro();
+            } else {
+              console.log(chalk.red('You did not enter a valid city'));
+              console.log(chalk.red('please enter a valid city'));
+              console.log('');
+              intro();
             }
-        }, function(error, response, body){
-          if(error) {
-            console.log(error);
-            intro();
-          } else if (response.statusCode === 200) {
-            var object = JSON.parse(body);
-            console.log('Your status code is: ',response.statusCode);
-            console.log('The city name is: ', object.name);
-            console.log('The weather will have: ', object.weather[0].description);
-            console.log('The temperature is: ', object.main.temp -273, 'celcius');
-            console.log('The humidity is: ', object.main.humidity, '%');
-            console.log('');
-            intro();
-          } else {
-            console.log('You did not enter a valid city');
-            console.log('please enter a valid city');
-            intro();
-          }
         });
       });
       break;
@@ -73,18 +85,24 @@ read.on('line',(line) => {
         if(answer.length < 140) {
           var results = "";
           tweet.post('statuses/update', { status: answer }, function(err, data, response) {
-            var object = data;
-            console.log('Your tweet has been posted. Here are some information');
-            console.log('Your tweet was posted at: ',object.created_at);
-            console.log('Your tweeted: ',object.text);
-            console.log('Your twitter name is: ',object.user.name);
-            console.log('What you do: ',object.user.description);
-            console.log('Your are at: ',object.user.time_zone);
-            intro();
+            if (error) {
+              console.log(chalk.red(error));
+            } else{
+              var object = data;
+              console.log(chalk.green('Your tweet has been posted. Here are some information'));
+              console.log(chalk.green('Your tweet was posted at: ',object.created_at));
+              console.log(chalk.green('Your tweeted: ',object.text));
+              console.log(chalk.green('Your twitter name is: ',object.user.name));
+              console.log(chalk.green('What you do: ',object.user.description));
+              console.log(chalk.green('Your are at: ',object.user.time_zone));
+              console.log('');
+              intro();
+            } 
           })
         } else {
-          console.log('');
-          console.log('Your text is longer than 140 characters');
+          console.log(chalk.red(''));
+          console.log(chalk.red('Your text is longer than 140 characters'));
+          console.log(chalk.red(''));
           console.log('');
           intro();
         }
@@ -92,10 +110,12 @@ read.on('line',(line) => {
       break;
 
     case 'exit':
+      console.log(chalk.blue('Thanks for using this application'));
       read.close();
       break;
 
     default:
+      console.log('');
       intro();
   }
 });
